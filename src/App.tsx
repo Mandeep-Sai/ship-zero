a6d00ce6c7373106d9e7e3572d5c9482f5a7a793
Mandeep Sai <bandimandeep7@gmail.com>
2023-07-14 15:00:31 +0200

    feat: intial setup and install necessary dependencies

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+function App() {
+  return (
+    <>
+      <h1>App</h1>
+    </>
+  );
+}
+
+export default App;
