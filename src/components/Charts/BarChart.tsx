43a4b5f94511b92d93a59d6ba58325cf8e093113
Mandeep Sai <bandimandeep7@gmail.com>
2023-07-17 10:36:35 +0200

    feat: install date picker and implement data filtering on bar chart

--- src/components/Charts/BarChart.tsx
+++ src/components/Charts/BarChart.tsx
@@ -7,6 +7,8 @@ import {
 } from "../../utilities/barChartConfig";
 import { filterBarChartData } from "../../utilities/utils";
 import "./BarChart.css";
+import "react-datepicker/dist/react-datepicker.css";
+import DatePicker from "react-datepicker";
 
 import { Chart as ChartJS, registerables, scales } from "chart.js";
 // import { Chart } from "react-chartjs-2";
@@ -21,14 +23,47 @@ const BarChart = ({ data }: BarChartProps) => {
   const [startYear, setStartYear] = useState<number>(2014);
   const [endYear, setEndYear] = useState<number>(2023);
 
+  const onChangeStartYear = (year: Date | null) => {
+    console.log(year);
+    if (year) {
+      setStartYear(year?.getFullYear());
+    }
+  };
+  const onChangeEndYear = (year: Date | null) => {
+    console.log(year);
+    if (year) {
+      setEndYear(year?.getFullYear());
+    }
+  };
+
   useEffect(() => {
     const filteredBarChartData = filterBarChartData(data, startYear, endYear);
     console.log(filteredBarChartData);
     setChartData(filteredBarChartData);
-  }, [data]);
+  }, [data, startYear, endYear]);
 
   return (
     <div className="barChart_wrapper">
+      <div className="yearRangePicker_wrapper">
+        <DatePicker
+          selected={new Date(startYear, 0, 1)}
+          onChange={(date: Date) => onChangeStartYear(date)}
+          selectsStart
+          startDate={new Date(startYear, 0, 1)}
+          endDate={new Date(endYear, 0, 1)}
+          dateFormat="yyyy"
+          showYearPicker
+        />
+        <DatePicker
+          selected={new Date(endYear, 0, 1)}
+          onChange={(date: Date) => onChangeEndYear(date)}
+          selectsEnd
+          startDate={new Date(startYear, 0, 1)}
+          endDate={new Date(endYear, 0, 1)}
+          dateFormat="yyyy"
+          showYearPicker
+        />
+      </div>
       <Bar
         data={barChartConfigData(chartData)}
         // options={{ maintainAspectRatio: false }}
